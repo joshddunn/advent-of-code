53d5ffbc99cde545f7f91ade1181a16df3b11cb1
joshddunn <joshdunn.uwo@gmail.com>
2021-12-04 03:02:04 -0500

    add day 5

---
+++ spec/5/solution_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../../5/solution'
+
+RSpec.describe '5' do
+  it 'example one' do
+    expect(part_one('example.txt')).to eq(0)
+  end
+
+  it 'part one' do
+    expect(part_one('input.txt')).to eq(0)
+  end
+
+  it 'example one' do
+    expect(part_two('example.txt')).to eq(0)
+  end
+
+  it 'part two' do
+    expect(part_two('input.txt')).to eq(0)
+  end
+end
