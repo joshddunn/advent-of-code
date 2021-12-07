6e4a44bb2f0fdd5ac59fe7d875fdaf52935d46eb
joshddunn <joshdunn.uwo@gmail.com>
2021-12-07 00:11:58 -0500

    day 7

--- 7/solution.rb
+++ 7/solution.rb
@@ -1,12 +1,19 @@
 require 'pry'
 
 def input(filename)
-  File.read("#{File.dirname(__FILE__)}/#{filename}").split("\n").map do |line|
-    # put stuff here
-    line
-  end
+  File.read("#{File.dirname(__FILE__)}/#{filename}").split("\n").first.split(",").map(&:to_i)
 end
 
-def solution(filename)
+def solution(filename, alternate = false)
   inputs = input(filename)
+
+  min = inputs.min
+  max = inputs.max
+
+  (min..max).map do |val|
+    inputs.sum do |input|
+      amount = (input - val).abs
+      alternate ? amount * (amount + 1) / 2 : amount
+    end
+  end.min
 end
--- spec/7/solution_spec.rb
+++ spec/7/solution_spec.rb
@@ -2,18 +2,18 @@ require_relative '../../7/solution'
 
 RSpec.describe '7' do
   it 'example one' do
-    expect(solution('example.txt')).to eq(0)
+    expect(solution('example.txt')).to eq(37)
   end
 
   it 'part one' do
-    expect(solution('input.txt')).to eq(0)
+    expect(solution('input.txt')).to eq(331067)
   end
 
   it 'example one' do
-    expect(solution('example.txt')).to eq(0)
+    expect(solution('example.txt', true)).to eq(168)
   end
 
   it 'part two' do
-    expect(solution('input.txt')).to eq(0)
+    expect(solution('input.txt', true)).to eq(92881128)
   end
 end
