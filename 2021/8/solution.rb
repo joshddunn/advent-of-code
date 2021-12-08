f1c1a8308937e45c7174cbc4acce7d6ffefe79b2
joshddunn <joshdunn.uwo@gmail.com>
2021-12-08 04:35:48 -0500

    add fast solution

--- 2021/8/solution.rb
+++ 2021/8/solution.rb
@@ -19,7 +19,7 @@ def input(filename)
   lines(__FILE__, filename).map do |line|
     codes, answer = line.split(" | ")
     OpenStruct.new(
-      codes: codes.split(" "),
+      codes: codes.split(" ").sort { |a, b| a.length <=> b.length },
       answer: answer.split(" ")
     )
   end
@@ -38,6 +38,38 @@ def solve(input)
 end
 
 def solution(filename, alternate = false)
-  solutions = input(filename).map { |input| solve(input) }
+  solutions = input(filename).map { |input| solve_fast(input) }
   alternate ? solutions.sum { |num| num.join("").to_i } : solutions.flatten.filter { |num| [1,4,7,8].include? num }.count
+end
+
+#####################
+# fast alt solution #
+#####################
+
+def solve_fast(input)
+  # first and second mapping
+  permutations = input.codes[0].split("").permutation(2).map { |p| p.zip(["c", "f"]) }
+
+  # third mapping
+  three = input.codes[1].tr(input.codes[0], "")
+  permutations.map! { |p| p.push [three, "a"] }
+
+  # fourth and fifth mapping
+  four = input.codes[2].tr(input.codes[0], "").split("").permutation(2).to_a
+
+  permutations.map! { |p| four.map { |f| p + f.zip(["b", "d"]) } }.flatten!(1)
+
+  # sixth and seventh mapping
+  codes = "abcdefg".tr(permutations[0].map(&:first).join, "").split("")
+  answers = "abcdefg".tr(permutations[0].map(&:last).join, "").split("")
+  pls = codes.permutation(2).map { |p| p.zip(answers) }
+
+  permutations.map! { |p| [p + pls[0], p + pls[1]] }.flatten!(1)
+
+  # solve
+  permutations.each do |permutation|
+    if input.codes.all? { |code| NUMBERS[code.tr(permutation.map(&:first).join, permutation.map(&:last).join).split("").sort.join] }
+      return input.answer.map { |answer| NUMBERS[answer.tr(permutation.map(&:first).join, permutation.map(&:last).join).split("").sort.join] }
+    end
+  end
 end
